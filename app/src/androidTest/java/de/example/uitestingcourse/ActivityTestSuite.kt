1929d61cacba4e3419ad038d2a4d0de30ab5c298
Fahri MacBook <fahri-can@web.de>
2020-11-15 20:55:07 +0100

    Commit rest of project.

---
+++ app/src/androidTest/java/de/example/uitestingcourse/ActivityTestSuite.kt
@@ -0,0 +1,12 @@
+package de.example.uitestingcourse
+
+import org.junit.runner.RunWith
+import org.junit.runners.Suite
+
+
+@RunWith(Suite::class)
+@Suite.SuiteClasses(
+    MainActivityTest::class,
+    SecondaryActivityTest::class
+)
+class ActivityTestSuite
